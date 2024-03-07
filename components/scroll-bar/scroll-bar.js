69219a1de48d1de7cb359b792baa09012f193a75
Trieu Khang Trat <96287500+KayT256@users.noreply.github.com>
2024-03-07 12:05:22 -0600

    Customize scrollbar

---
+++ components/scroll-bar/scroll-bar.js
@@ -0,0 +1,14 @@
+// Only when scrolling that the scrollbar is displayed. Otherwise, hide it.
+
+// window.addEventListener('load', () => {
+//     let timer;
+//     document.querySelectorAll('.scroll').forEach(element => {
+//         element.addEventListener('scroll', () => {
+//             element.classList.add('show-scroll');
+//             timer = setTimeout(timer);
+//             timer = setTimeout(() => {
+//                 element.classList.remover('show-scroll')
+//             }, 1000)
+//         })
+//     })
+// })
